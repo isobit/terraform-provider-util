a2346a1f08a08f6ea4dc3889ca762f657c432065
Josh Glendenning <git@isobit.io>
2024-08-19 13:00:42 -0500

    Update example

--- examples/resources/util_indestructible/resource.tf
+++ examples/resources/util_indestructible/resource.tf
@@ -1,22 +1,20 @@
 # Example of a resource that will be protected from destruction.
-resource "terraform_data" "protected" {
-	# It is still important to configure the lifecycle with prevent_destroy to
-	# avoid destruction if the plan requires replacement. Note that
-	# prevent_destroy alone does not prevent destruction when the entire config
-	# is removed; util_indestructible.this is needed to prevent that.
-	lifecycle {
-		prevent_destroy = true
-	}
-}
+resource "terraform_data" "protected" {}
 
 # This indestructible resources protects the resource from destruction by
 # depending on it; if terraform tries to destroy the proected resource, it will
 # destroy util_indestructible.this first, which will fail unless allow_destroy
 # is set to true.
 resource "util_indestructible" "this" {
-	depends_on = [
-		terraform_data.protected,
-	]
+	# The protected value can be any attribute of the protected resource that
+	# would change under replacement. The resource can be also be protected using
+	# an explicit "depends_on", but in that case the resource also needs to have
+	# "prevent_destroy = true" in the lifecycle configuration to prevent
+	# destruction during replacement. Using protected_values simultaneously
+	# implies the dependency relationship, and protects against destruction
+	# during replacement by causing the indestructible resource itself to also be
+	# replaced when the protected resource is replaced.
+	protected_value = terraform_data.protected.id
 	# Custom messages are optional, but helpful to explain why it is important to
 	# avoid destroying the protected resources.
 	error_message = "Destroying terraform_data.protected will cause major issues, please don't destroy it!"
